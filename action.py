9dc838f3d250e5cca94d1a23a9d27ddf680b69c5
thwhite <thomaswhite4@gmail.com>
2020-10-25 15:58:40 -0700

    Got started on action.py

---
+++ action.py
@@ -0,0 +1,37 @@
+import random
+
+class Action:
+
+    def __init__(self, *kwargs):
+        self.actor = kwargs["Actor"]
+        self.target = kwargs["Target"]
+        self.attack_roll = kwargs["attack_roll"]
+        self.save_stat = kwargs["save_stat"]
+
+
+
+
+    def resolve_action(self):
+        attack = random.randint(1, self.attack_roll)
+        if attack >= self.target.states["AC"]:
+            defend = self.target.stats["save_stat"] #This format is wrong rihgt now, will figure later
+            if random.randint(1, defend) <= save_stat:
+                # how to handle actions. is it true that every action increments or decremets a stat of some form?
+
+
+
+
+self.agent
+    self.foe
+    self.dice_roll
+    self.modifier
+    self.effect <-- which state(s) changes?
+    ... etc
+    what is the recipe for an action?
+        1. roll dice, add modifiers
+        2. compare rolls/AC/etc.
+        3. roll damage
+        4. calculate state updates
+
+    functions:
+        - resolve_action() returns delta_states
