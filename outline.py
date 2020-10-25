d7c715ad88d7f57d439cebad73e17cfd74e1e450
Valerie <valerie@osmobot.com>
2020-10-25 08:28:29 -0700

    Added some Valerie musings to outline.py

--- outline.py
+++ outline.py
@@ -9,35 +9,53 @@ def metrics()
 
 ---
 policy=list of actions ???
+where are agent(s), foe(s) initiated? MC/Test?
 
 
-def reward(characters)
+def reward([characters])
     - note: can use type(character) to determine positive or negative reward
     calculate the reward??
 
-def branch_and_bound()
+def branch_and_bound(depth=int, [agents], foe)
+    - Am I recursive? If so, Valerie's gonna be sad
+
+    subfunctions:
+        - lookahead
+
+    return policy?
 
 
-def encounter()
+
+def encounter([agents], foe)
+
+    return ?
 
 
 def turn(action)
 
     agent_action = agent.act(action)
     foe_action = foe.act()
-    for action in action_objects
+    for action in [actions]
         delta_states = action.resolve_action()
         update agent.states, etc.
         update foe.states, etc.
     foe.decrement_cooldowns()
 
+    return ?
 
-class action_object (todo: better name please!)
+
+class action()
+    self.agent
+    self.foe
     self.dice_roll
-    self... etc
+    self.modifier
+    self.effect <-- which state(s) changes?
+    ... etc
     what is the recipe for an action?
-        1. roll die, add modifier
-        2. ...
+        1. roll dice, add modifiers
+        2. compare rolls/AC/etc.
+        3. roll damage
+        4. calculate state updates
 
     functions:
         - resolve_action() returns delta_states
@@ -45,7 +63,7 @@ class action_object (todo: better name please!)
 
 class agent()
     self.hp = int
-    self.stats = list of 6 ints
+    self.stats = [6 ints]
     self.states = dict(AC, protections, spell_slots)
     self.actions = dict(action: availability)
 
@@ -55,7 +73,7 @@ class agent()
 
 class foe()
     self.hp = int
-    self.stats = 6x1 array
+    self.stats = [6 ints]
     self.states = dict(AC, protections, debuffs, cooldowns)
     self.actions = dict(action: availability)
 
