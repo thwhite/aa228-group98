2f4c1e8b92c87bbbfd5aca35a4aea49fc4b3fbb8
Valerie <valerie.pietrasz@gmail.com>
2020-10-26 17:25:28 -0700

    Cleaned up outline.py with typing

--- outline.py
+++ outline.py
@@ -9,37 +9,44 @@ def metrics()
 
 ---
 
-policy=dict(action: dict(state: value)) <-- Ask @ OH
+class Reward:
+    self.reward_for_kill: float = 1000,
+    self.penalty_for_dying: float = -1000,
+    self.agent_hp_bonus: float = 10,
+    self.agents: [Agent] = [Agent()],
+    self.foe: Foe = Foe(),
+
+    functions:
+        - get_reward(agents: [Agent] = [Agent()], foe: Foe = Foe()) -> reward: float
 
 
-def reward([agents], foe, turn_count)
-
-    reward_for_dead_foe, penalty_for_dying, penalty_for_hp_loss
-    if-then statements
-
-    return reward
-
-
-def branch_and_bound(bb_weight, depth=int, [agents_kwargs], [policies], foe_kwargs)
-    - Am I recursive? If so, Valerie's gonna be sad [Yes]
+def branch_and_bound(
+    bb_weight: float = 1,
+    depth: int = 3,
+    discount: float = 0.9,
+    agent_kwargs: [kwargs],
+    MC_policy,
+    foe_kwargs: [kwargs],
+    ): -> action: Action, reward: float
 
     subfunctions:
-        - lookahead
-
-    return [policies], reward
+        - lookahead()
 
 
-def encounter([agents_kwargs], [policies], foe_kwargs, turn_limit)
-    initiates [agents], foe
-    - note: for now, agent(s) act first?
+def encounter(
+    agents_kwargs: [kwargs],
+    policies: [policy],
+    foe_kwargs: [kwargs],
+    turn_limit: int
+    ): -> agents: [Agent], foe: Foe, turn_count: int
 
-    calls turn(action) <- action pulled from policy
-    update [agents], foe
+    Initiates [agents], foe
+        Agent(s) act first? (for now)
+    Calls turn(action) <- action pulled from policy
+    Updates [agents], foe
 
-    return [agents], foe, turn_count
 
-
-def turn(action)
+def turn(action: Action) -> ?
 
     agent_action = agent.act(action)
     foe_action = foe.act()
@@ -49,15 +56,13 @@ def turn(action)
         update foe.states, etc.
     foe.decrement_cooldowns()
 
-    return ?
 
-
-class action()
+class Action:
     self.actor
     self.targets
     self.dice_roll
-    self.modifier <-- which
-    self.effect <-- which state(s) changes?
+    self.modifier # <-- which modifier to use
+    self.effect # <-- which state(s) changes?
     ... etc
     what is the recipe for an action?
         1. roll dice, add modifiers
@@ -69,24 +74,52 @@ class action()
         - resolve_action() returns delta_states
 
 
-class agent()
-    self.max_hp = int
-    self.hp = int
-    self.stats = [6 ints]
-    self.states = dict(AC, protections, spell_slots)
-    self.actions = dict(action: availability)
+class Agent:
+    self.name: str
+    self.max_hp: int
+    self.hp: int
+    self.stats: [int] # length 6
+    self.states: dict(AC, protections, spell_slots)
+    self.actions: dict(action: availability)
 
     functions:
         - act(action) returns action object
 
 
-class foe()
-    self.max_hp = int
-    self.hp = int
-    self.stats = [6 ints]
-    self.states = dict(AC, protections, debuffs, cooldowns)
-    self.actions = dict(action: availability)
+class Foe:
+    self.max_hp: int
+    self.hp: int
+    self.stats: [int] # length 6
+    self.states: dict(AC, protections, debuffs, cooldowns)
+    self.actions: dict(action: availability)
 
     functions:
         - act() returns action object
         - decrement_cooldowns()
+
+
+--- scratch/notes ---
+
+# define an array states, give each state some index, function maps state to idx and vice versa
+# repeat for actions
+# policy: array where index is state index, value is action index
+#
+#
+# class DungeonsAndDragonsMDP:
+#
+# def DungeonState: # RECALL EXPONENTIAL GROWTH OF STATE-SPACE
+#     def __init__(self,...):
+#         agent_hp
+#         agent_ac
+#         ...
+#         foe_hp
+#         foe_ac
+#
+# def state_to_index(self, state):
+#     idx = 10*state.hp + 10*state.ac
+#     return idx
+#
+# def index_to_state(self,idx):
+#     hp = np.mod(idx,self.MAX_HP) #
+#     ac = ....
+#     return State(hp,ac)
