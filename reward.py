07a99aa89b9dc1b55808605ff9fdd41e46a85735
Valerie <valerie.pietrasz@gmail.com>
2020-11-02 15:11:49 -0800

    Stripped reward.py of ability to handle multiple agents

--- reward.py
+++ reward.py
@@ -6,15 +6,14 @@ class Reward:
     reward_for_kill: float = 1000,
     penalty_for_dying: float = -1000,
     agent_hp_bonus: float = 10,
-    agents: [Agent] = [Agent()],
+    agent: Agent = Agent(),
     foe: Foe = Foe(),
     ):
-        reward_per_agent_hp = {}
-        for agent in agents
-            reward_per_agent_hp{agent} = agent_hp_bonus*np.arange(
+
+        reward_per_agent_hp = agent_hp_bonus*np.arange(
                 start=0, stop=agent.max_hp
-            )
-            reward_per_agent_hp{agent}[0] = penalty_for_dying
+        )
+        reward_per_agent_hp[0] = penalty_for_dying
 
         reward_per_foe_hp = [0]*foe.max_hp
         reward_per_foe_hp[0] = reward_for_kill
@@ -23,13 +22,9 @@ class Reward:
         self.reward_per_foe_hp = reward_per_foe_hp
 
 
-    def get_reward(self, agents: [Agent], foe: Foe) -> reward: float:
+    def get_reward(self, agent: Agent, foe: Foe) -> reward: float:
 
-        reward = sum([
-            self.reward_per_agent_hp{agent}[agent.hp]
-            for agent in agents
-        ]) # This line might not work; we'll see
-
-        reward = reward + self.reward_per_foe_hp[foe.hp]
-
-        return reward
+        return (
+            self.reward_per_agent_hp[agent.hp]
+            + self.reward_per_foe_hp[foe.hp]
+        )
