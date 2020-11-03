5c192dfc7e7e30144606e2e355c76892b1c1d0e0
Valerie <valerie.pietrasz@gmail.com>
2020-11-03 10:05:32 -0800

    Theoretically finished DungeonState.py -- untested still

--- dungeonstate.py
+++ dungeonstate.py
@@ -4,14 +4,14 @@ class DungeonState:
 
     # BIG TODO: test
 
-  def __init__(self, initial_agent: Agent, initial_foe: Foe):
-      # Note: This assumes that all states start at their max possible value,
-      # and further that they are ints that cannot go negative.
-      # TODO: Add this note to agent/foe.py
-      self.available_agent_states = agent.states
-      self.available_agent_hp = agent.hp
-      self.available_foe_states = foe.states
-      self.available_foe_hp = foe.hp
+    def __init__(self, initial_agent: Agent, initial_foe: Foe):
+        # Note: This assumes that all states start at their max possible value,
+        # and further that they are ints that cannot go negative.
+        # TODO: Add this note to agent/foe.py
+        self.available_states = {
+            'agent': {*initial_agent.states, initial_agent.hp},
+            'foe': {*initial_foe.states, initial_foe.hp},
+      }
 
     def agent_foe_to_index(self, agent: Agent, foe: Foe): -> idx: int
 
@@ -32,28 +32,28 @@ class DungeonState:
 
     def index_to_states(self, idx): -> state: dict
 
-        available_states = self.available_states_dict()
+        states = {}
 
-        for ## Valerie, you're here
+        for a in ['agent', 'foe']:
+            for state, max_state in self.available_states[a]:
+                a_states{state} = idx % (max_state + 1)
+                idx //= (max_state + 1)
+            states{a} = a_states
 
-
-        # wrap agent.hp into agent.states
-        return {agent: agent.states, foe: foe.states}
+        return states
 
 
     def __align_state_and_dim_arrays(self, states):
 
-        available_states = self.available_states_dict()
-
         state_array = [
             states[a][s]
-            for s in states[a].keys() & available_states[a].keys()
+            for s in states[a].keys() & self.available_states[a].keys()
             for a in ['agent', 'foe']
         ]
 
         state_dims = [
             available_states[a][s]
-            for s in states[a].keys() & available_states[a].keys()
+            for s in states[a].keys() & self.available_states[a].keys()
             for a in ['agent', 'foe']
         ]
 
@@ -69,10 +69,3 @@ class DungeonState:
                 for i, state in enumerate(states)
             ]
         )
-
-    def available_states_dict(self): -> dict
-
-        return {
-            'agent': self.available_agent_states,
-            'foe': self.available_foe_states
-        }
