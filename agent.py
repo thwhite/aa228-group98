3ce7dc94916980b17d8678a81ce02be061804944
thwhite <thomaswhite4@gmail.com>
2020-11-02 16:28:40 -0800

    fixed typos in agent and foe (thanks) and worked on handling actions

--- agent.py
+++ agent.py
@@ -3,10 +3,11 @@ class Agent:
     def __init__(self):
         self.hp = 12
         self.stats = [14, 14, 14, 14, 14, 14]
-        self.states = {"AC", 15, "absorb", 1} # Thomas I think this needs to be {"AC": 15}, etc -- Valerie
-        self.actions = {"don", 1, "doff", 0, "absorb", 1, "claws", 1, "healing", 1, "moonbeam", 1, "spell slots", 6}
+        self.states = {"AC": 15, "absorb": 1} # Thomas I think this needs to be {"AC": 15}, etc -- Valerie
+        self.actions = {"don": 1, "doff": 0, "absorb": 1, "claws": 1, "healing": 1, "moonbeam": 1, "spell slots": 6}
 
-    def act(input):
-        action = Action()
+    def act(input, policy):
+        # do I put an if/else tree here?!? Needs to be parametricized.
+        action = Action(policy)
 
         return action
--- foe.py
+++ foe.py
@@ -1,13 +1,24 @@
+import random
+
 class Foe:
 
     def __init__(self):
         self.hp = 127
         self.stats = [18, 19, 17, 14, 16, 15]
-        self.states = {"AC", 20, "buffed", 0, "cooldown", 4}
-        self.actions = {"hit_small", 1, "hit_large", 1, "buff", 1, "radiant_breath", 1}
+        self.states = {"AC": 20, "buffed": 0, "cooldown": 4}
+        self.actions = {"hit_small": 1, "hit_large": 1, "buff": 1, "radiant_breath": 1}
 
-    def act(self, input):
-        action = Action()
+    def act(self):
+        e = random.random()
+
+        if e > 0.7:
+            # choose a random action and do it
+        else:
+            if self.actions["radiant_breath"]:
+                action = Action()
+            elif self.actions["buff"]:
+                action = Action()
+            elif self.actions["hit_large"]
 
         if self.actions["radiant_breath"]:
 
@@ -20,4 +31,6 @@ class Foe:
     def decrement_cooldowns(self):
         if self.states["cooldown"] >= 1:
             self.states["cooldown"] = self.states["cooldown"] - 1
+        elif not self.states["cooldown"]:
+            self.actions["radiant_breath"] = 1
 
