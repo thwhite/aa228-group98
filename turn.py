12d59e33ce43eeca638fa18d9d471368d46335f6
thwhite <thomaswhite4@gmail.com>
2020-11-02 17:08:54 -0800

    Outlined turn.py. Primary question for me is exact structure of delta_states.

---
+++ turn.py
@@ -0,0 +1,21 @@
+def turn(agent, foe, policy):
+
+    actions = []
+    actions.append = agent.act(policy)
+    actions.append = foe.act()
+
+    for action in actions:
+        # Primary note: if we're going to resolve these abstractly, need a sense of the target to exit this.
+        # Can delta_states be of length greater than one?
+        # Strongly depends on structure of delta_states
+
+        delta_states = action.resolve_action()
+        target = delta_states.target()
+        for i in len(delta_states.values):
+            target.state[delta_states[i].key] = delta_states[i].value
+
+    foe.decrement_cooldowns()
+
+
+
+
