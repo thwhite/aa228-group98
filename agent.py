220d740363777d96e5a466e0ce6ca7f184affe0c
thwhite <thomaswhite4@gmail.com>
2020-11-02 18:43:01 -0800

    New structure for agent

--- agent.py
+++ agent.py
@@ -2,31 +2,35 @@ class Agent:
 
     def __init__(self):
         self.hp = 12
-        self.stats = [14, 14, 14, 14, 14, 14]
-        self.states = {"AC": 15, "absorb": 1, "spell slots": 6}
-        self.actions = {"hit": 1, "harder_hit": 1, "don": 1, "doff": 0, "absorb": 1, "claws": 1, "healing": 1, "moonbeam": 1}
+        self.stats = [14, 14, 14, 14, 14, 14, 15] # six traditional stats and AC
+        self.states = {"shield": 1, "absorb": 1, "spell slots": 6}
 
-    def act(self, policy):
+    def get_available_actions(self):
+        actions = ["hit", "toggle_shield"]
+        if not self.states["shield"]:
+            actions.append("harder_hit")
+        if self.states["spell_slots"] is not 0:
+            actions.append(["absorb", "claws", "healing", "moonbeam"])
+        return actions
 
-        # What if policy asks for an action that can't be done?
-        # Non-parametric nature of the action space is actually kinda a problem.
-        # A stupid approach here is to have the policy default to an
+    def act(self, policy_step):
 
-        if policy == 0 and self.actions["don"]:
-            action = Action() # don
-        elif policy == 1 and self.actions["doff"]:
-            action = Action() # doff
-        elif policy == 2 and self.actions["absorb"] and self.states["spell slots"] is not 0:
-            action = Action() # absorb
-        elif policy == 4 and self.actions["claws"] and self.states["spell slots"] is not 0:
-            action = Action() # claws
-        elif policy == 5 and self.actions["healing"] and self.states["spell slots"] is not 0:
-            action = Action() # healing
-        elif policy == 6 and self.actions["moonbeam"] and self.states["spell slots"] is not 0:
-            action = Action() # moonbeam
-        elif policy == 7 and self.actions["harder_hit"]:
+        if policy_step == "toggle_shield":
+            self.states["shield"] = not self.states["shield"] # flips the bit
             action = Action()
-        else:
+        elif policy_step == "absorb":
+            action = Action() # absorb
+        elif policy_step == "claws":
+            action = Action() # claws
+        elif policy_step == "healing":
+            action = Action() # healing
+        elif policy_step == "moonbeam":
+            action = Action() # moonbeam
+        elif policy_step == "harder_hit":
+            action = Action()
+        elif: policy_step == "hit":
             action = Action() # hit
+        else:
+            action = Action() # default
 
         return action
