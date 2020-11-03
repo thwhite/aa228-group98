249ef4937ea8a83f1d57a50bc6cadbbe2d09c203
thwhite <thomaswhite4@gmail.com>
2020-11-02 16:41:16 -0800

    Outlined foe decison-making approach

--- foe.py
+++ foe.py
@@ -5,27 +5,30 @@ class Foe:
     def __init__(self):
         self.hp = 127
         self.stats = [18, 19, 17, 14, 16, 15]
-        self.states = {"AC": 20, "buffed": 0, "cooldown": 4}
+        self.states = {"AC": 20, "buffed": 0, "radiant_cooldown": 4, "hit_large_cooldown": 1}
         self.actions = {"hit_small": 1, "hit_large": 1, "buff": 1, "radiant_breath": 1}
 
     def act(self):
         e = random.random()
 
-        if e > 0.7:
-            # choose a random action and do it
+        # Note: turns out choosing a random action is remarkably hard to do since they're all in a dict
+        # and the specific details have to be specified into the action object.
+
+        # Current implementation has random chance to taking default action even when inadviseable. This is less
+        # than we were hoping but hopefully ok for MVP.
+
+        if self.actions["radiant_breath"] and e < 0.7:
+            action = Action()
+            self.actions["radiant_breath"] = 0
+            self.states["radiant_cooldown"] = 4
+        elif self.actions["hit_large"] and e < 0.7:
+            action = Action()
+            self.actions["hit_large"] = 0
+            self.states["hit_large_cooldown"] = 1
+        elif self.actions["buff"] and e < 0.7:
+            action = Action()
         else:
-            if self.actions["radiant_breath"]:
-                action = Action()
-            elif self.actions["buff"]:
-                action = Action()
-            elif self.actions["hit_large"]
-
-        if self.actions["radiant_breath"]:
-
-        elif self.actions["buff"]:
-
-
-
+            action = Action()
         return action
 
     def decrement_cooldowns(self):
