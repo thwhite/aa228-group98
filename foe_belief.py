474ad5137bbb9f62a62e618f8556d9a9818bdc9d
thwhite <thomaswhite4@gmail.com>
2020-11-08 19:47:15 -0800

    Now foe belief runs.

--- foe_belief.py
+++ foe_belief.py
@@ -9,7 +9,7 @@ def update_foe_belief(faux_foe: Foe, agent_action, foe_reaction: str):
 
     print(f'E(damage): {expected_damage}')
 
-    if foe_reaction[0:2] == "RAW":
+    if "RAW" in foe_reaction:
         # @Thomas: Do we also need to update what we think the faux foe's max hp is?
         # What about AC?
         faux_foe.hp = faux_foe.max_hp*(len(foe_reaction) - 3)*0.1
