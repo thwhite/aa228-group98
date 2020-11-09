0ab99006a9c3a2f4e234601f991237781317e473
thwhite <thomaswhite4@gmail.com>
2020-11-08 20:17:44 -0800

    Ok I think this works now.

--- foe_belief.py
+++ foe_belief.py
@@ -8,11 +8,11 @@ def update_foe_belief(faux_foe: Foe, agent_action, foe_reaction: str):
     # expected_damage = faux_foe.hp - expected_states
 
     # print(f'E(damage): {expected_damage}')
-    print(foe_reaction)
 
     if "RAW" in foe_reaction:
         # @Thomas: Do we also need to update what we think the faux foe's max hp is?
         # What about AC?
-        faux_foe.hp = faux_foe.max_hp*(len(foe_reaction) - 3)*10
+        print(foe_reaction)
+        faux_foe.hp = (14 - (len(foe_reaction) - 3))*10
 
     return faux_foe
