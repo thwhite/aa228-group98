7c71f55e577aa0ebd0b8583177af43eb324fc991
thwhite <thomaswhite4@gmail.com>
2020-11-08 19:49:49 -0800

    Ok now I think the foe damage works sorta.

--- foe_belief.py
+++ foe_belief.py
@@ -5,13 +5,13 @@ from foe import Foe
 def update_foe_belief(faux_foe: Foe, agent_action, foe_reaction: str):
 
     expected_states = agent_action.action_expectation(faux_foe)
-    expected_damage = faux_foe.hp - expected_states["target"]["hp"]
+    expected_damage = faux_foe.hp - expected_states
 
     print(f'E(damage): {expected_damage}')
 
     if "RAW" in foe_reaction:
         # @Thomas: Do we also need to update what we think the faux foe's max hp is?
         # What about AC?
-        faux_foe.hp = faux_foe.max_hp*(len(foe_reaction) - 3)*0.1
+        faux_foe.hp = faux_foe.hp*(len(foe_reaction) - 3)*0.1
 
     return faux_foe
